484379ddf5f746e56e22a5ebce474480b0740679
Dmitry Razumov <Dmytro.Razumov@tietoevry.com>
2025-08-17 15:50:46 +0300

    Starting business logic implementation

--- src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.polarbookshop.catalog_service;
+package com.polarbookshop.catalogservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +8,7 @@ class CatalogServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+        // will be implemented later
 	}
 
 }
